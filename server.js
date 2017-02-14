6185cd9fec6bd9092a7517db2c28b9b77ecc1bd1
Benny kedmi <bennykedmi@gmail.com>
2017-02-14 09:42:10 +0200

    Add ?

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ app.get('/',function(req,res){
 	res.send('Todo API Root');
 });
 
+//GET /todos?completed=true
+app.get('/todos',function(req,res){
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos,{completed:true});
+	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos,{completed:false});
+	}
+	
+	res.send(filteredTodos);
+});
+
 //GET /todos/:id
 
 app.get('/todos',function(req,res){
