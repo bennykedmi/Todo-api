a0e62198b391960d0f6fd9f0cbfe1771263b3834
Benny kedmi <bennykedmi@gmail.com>
2017-02-15 06:35:21 +0200

    Final static todos

--- server.js
+++ server.js
@@ -23,15 +23,21 @@ app.get('/todos',function(req,res){
 		filteredTodos = _.where(filteredTodos,{completed:false});
 	}
 	
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos,function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+	
 	res.send(filteredTodos);
 });
 
-//GET /todos/:id
 
 app.get('/todos',function(req,res){
 	res.json(todos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id',function(req,res){
 	var todoId = parseInt(req.params.id,10)
 	var matchedTodo = _.findWhere(todos,{id:todoId});
